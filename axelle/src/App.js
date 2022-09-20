6e151b430d2c1b934c08140a64dda74021f122e9
Vincent Raveneau <vincentraveneau@MacBook-Pro-de-Vincent.local>
2022-09-20 13:39:25 +0200

    finish

--- axelle/src/App.js
+++ axelle/src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components'
+
 
 function App() {
+
+  const audio = new Audio('HB.mp3');
+
+  const Start = () => {
+    audio.play()
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="axelle.png" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          T'es une ouf mais Joyeux anniversaire grande folle !
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Lien onClick={Start} href="Places_axelle.pdf" download="Joyeux Anniversaire Axelle.pdf">Prend ton cadeau et rend pas fou</Lien>
       </header>
     </div>
   );
 }
 
+
+const Lien = styled.a`
+  display: flex;
+  color: #348ceb;
+  font-size: 40px;
+`
 export default App;
